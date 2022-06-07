3b229e64a93c5e8e91ea2fcbb4c432ba42d715e2
TonyDCoder5 <tonyaflex5@gmail.com>
2022-06-07 13:05:45 -0600

    module 35.7 edits - async/await error handling/refactor

--- src/categories/categories.controller.js
+++ src/categories/categories.controller.js
@@ -1,12 +1,11 @@
 const categoriesService = require("./categories.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
-function list(req, res, next) {
-  categoriesService
-    .list()
-    .then((data) => res.json({ data }))
-    .catch(next);
+async function list(req, res) {
+  const data = await categoriesService.list();
+  res.json({ data });
 }
 
 module.exports = {
-  list,
+  list: asyncErrorBoundary(list),
 };
--- src/products/products.controller.js
+++ src/products/products.controller.js
@@ -1,17 +1,25 @@
 const productsService = require("./products.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
+
+async function productExists(req, res, next) {
+  const product = await productsService.read(req.params.productId);
+  if (product) {
+    res.locals.product = product;
+    return next();
+  }
+  next({ status: 404, message: `Product cannot be found.` });
+}
 
 function read(req, res, next) {
   res.json({ data: { product_title: "some product title" } });
 }
-
-function list(req, res, next) {
-  productsService
-    .list()
-    .then((data) => res.json({ data }))
-    .catch(next);
+async function list(req, res, next) {
+  const data = await productsService.list();
+  res.json({ data });
 }
 
 module.exports = {
-  read: [read],
-  list: [list],
+  read: [asyncErrorBoundary(productExists), read],
+  list: asyncErrorBoundary(list),
 };
